f3d03969de32e456becf8acb26071bd6d4cc89b2
Ajith Hussain <csy0013@googlemail.com>
2017-07-06 21:32:21 +1000

    Updated for webdsl/html

--- reactor.go
+++ reactor.go
@@ -1,15 +1,11 @@
 package reactor
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
-	"strings"
 
-	"github.com/kirillrdy/nadeshiko/html"
-	"github.com/sparkymat/reactor/property"
-	"github.com/sparkymat/webdsl/css"
+	"github.com/sparkymat/webdsl/html"
 )
 
 type fileMapping struct {
@@ -19,49 +15,15 @@ type fileMapping struct {
 
 type reactor struct {
 	name                  string
-	skipReactorStartup    bool
 	javascriptFolders     []fileMapping
 	cssFolders            []fileMapping
 	customJavascriptLinks []string
 	customCssLinks        []string
-	props                 map[string]Property
 }
 
 func New(name string) reactor {
 	r := reactor{name: name}
-	r.props = make(map[string]Property)
 	return r
-}
-
-func (r *reactor) EnableReactStartup() {
-	r.skipReactorStartup = false
-}
-
-func (r *reactor) DisableReactStartup() {
-	r.skipReactorStartup = true
-}
-
-func (r *reactor) SetStringProperty(name string, value string) {
-	property := Property{propertyType: property.String, value: value}
-	r.props[name] = property
-}
-
-func (r *reactor) SetIntegerProperty(name string, value int64) {
-	property := Property{propertyType: property.Integer, value: fmt.Sprintf("%v", value)}
-	r.props[name] = property
-}
-
-func (r *reactor) SetFloatProperty(name string, value float64) {
-	property := Property{propertyType: property.Float, value: fmt.Sprintf("%v", value)}
-	r.props[name] = property
-}
-
-func (r *reactor) SetObjectProperty(name string, value interface{}) {
-	jsonValue, _ := json.Marshal(value)
-
-	// FIXME: Ignoring error for now
-	property := Property{propertyType: property.Object, value: strings.Replace(string(jsonValue), "\"", "\\\"\\", -1)}
-	r.props[name] = property
 }
 
 func (r *reactor) MapJavascriptFolder(file string, web string) {
@@ -80,10 +42,11 @@ func (r *reactor) AddCustomCssLink(link string) {
 	r.customCssLinks = append(r.customCssLinks, link)
 }
 
-func (r reactor) Html() html.Node {
-	headNodes := []html.Node{}
+func (r reactor) Html() html.HtmlDocument {
+	javascriptNodes := []*html.Node{}
+	headNodes := []*html.Node{}
 
-	headNodes = append(headNodes, html.Title().Text(r.name))
+	headNodes = append(headNodes, html.Title(r.name))
 
 	// List javascript files
 	for _, jsMap := range r.javascriptFolders {
@@ -92,14 +55,14 @@ func (r reactor) Html() html.Node {
 			for _, jsFile := range files {
 				if filepath.Ext(jsFile.Name()) == ".js" {
 					finalPath := fmt.Sprintf("/%v/%v", jsMap.webPath, jsFile.Name())
-					headNodes = append(headNodes, html.Script().Attribute("async", "async").Type("text/javascript").Src(finalPath))
+					javascriptNodes = append(javascriptNodes, html.Script().Attr("type", "text/javascript").Attr("src", finalPath))
 				}
 			}
 		}
 	}
 
 	for _, jsLink := range r.customJavascriptLinks {
-		headNodes = append(headNodes, html.Script().Type("text/javascript").Src(jsLink))
+		headNodes = append(headNodes, html.Script().Attr("type", "text/javascript").Attr("src", jsLink))
 	}
 
 	// List css files
@@ -110,34 +73,22 @@ func (r reactor) Html() html.Node {
 			for _, cssFile := range files {
 				if filepath.Ext(cssFile.Name()) == ".css" {
 					finalPath := fmt.Sprintf("/%v/%v", cssMap.webPath, cssFile.Name())
-					headNodes = append(headNodes, html.Link().Rel("stylesheet").Href(finalPath))
+					headNodes = append(headNodes, html.Link().Attr("rel", "stylesheet").Attr("href", finalPath))
 				}
 			}
 		}
 	}
 
 	for _, cssLink := range r.customCssLinks {
-		headNodes = append(headNodes, html.Link().Rel("stylesheet").Href(cssLink))
+		headNodes = append(headNodes, html.Link().Attr("rel", "stylesheet").Attr("href", cssLink))
 	}
 
-	propsList := []string{}
+	childNodes := []*html.Node{html.Div().Attr("id", "js-reactor-app")}
 
-	if r.props != nil {
-		for key, value := range r.props {
-			propsList = append(propsList, fmt.Sprintf("%v: %v", key, value.String()))
-		}
-	}
+	childNodes = append(childNodes, javascriptNodes...)
 
-	propsString := fmt.Sprintf("{%v}", strings.Join(propsList, ","))
-
-	childNodes := []html.Node{html.Div().Id(css.Id("app-container"))}
-
-	if r.skipReactorStartup == false {
-		childNodes = append(childNodes, html.Script().TextUnsafe(fmt.Sprintf("ReactDOM.render(React.createElement(%v, %v), document.getElementById('app-container'));", r.name, propsString)))
-	}
-
-	return html.Html().Children(
-		html.Head().Children(headNodes...),
-		html.Body().Children(childNodes...),
+	return html.Html(
+		html.Head(headNodes...),
+		html.Body(childNodes...),
 	)
 }
